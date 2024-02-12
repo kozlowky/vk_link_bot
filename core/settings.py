0bb1523836db7de711de3de1aef9316e3bb06ff4
Kozlow <kozlov.khv@gmail.com>
2024-02-12 12:55:05 +1000

    stiings update

--- core/settings.py
+++ core/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': env.str('DB_NAME'),
         'USER': env.str('DB_USER'),
         'PASSWORD': env.str('DB_PASSWORD'),
-        'HOST': env.str('DB_HOST'),
+        'HOST': env.str('DB_HOST', default='localhost'),
         'PORT': env.str('DB_PORT'),
     }
 }
