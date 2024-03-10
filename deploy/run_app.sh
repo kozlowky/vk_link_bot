426453fc872545e57d8ef51f3a3552a5e61d69f6
Kozlow <kozlov.khv@gmail.com>
2024-03-10 17:11:17 +1000

    update run_app.sh

--- deploy/run_app.sh
+++ deploy/run_app.sh
@@ -5,8 +5,8 @@ export PYTHONPATH="${PYTHONPATH}:/app"
 
 gunicorn -c "gunicorn.conf.py" backend.wsgi:application
 
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:8000 &
-python manage.py run_bot
+python core/manage.py collectstatic --noinput
+python core/manage.py migrate --noinput
+python core/manage.py runserver 0.0.0.0:8000 &
+python core/manage.py run_bot
 
