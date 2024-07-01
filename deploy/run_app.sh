52e111edd47283abee1db07735449ea44483d2ab
Kozlow <kozlov.khv@gmail.com>
2024-07-01 17:20:48 +1000

    run_app.sh update

--- deploy/run_app.sh
+++ deploy/run_app.sh
@@ -5,5 +5,5 @@ python "manage.py" migrate --noinput
 python "manage.py" runserver 0.0.0.0:8000 &
 python "manage.py" run_bot
 
-gunicorn -c "$PROJECT_ROOT/gunicorn.conf.py" src.wsgi:application
+gunicorn -c "$PROJECT_ROOT/gunicorn.conf.py" core.wsgi:application
 
